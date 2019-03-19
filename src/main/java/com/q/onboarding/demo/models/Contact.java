ee450b8fc72b570204e49502a9ea80e43118e5e2
Q <quintin.robinson@qrobinsonml1.infusionsoft.phx>
2019-03-19 12:51:58 -0700

    updated gradle to include missing depedencies (learning java/spring is interesting...)

--- src/main/java/com/q/onboarding/demo/models/Contact.java
+++ src/main/java/com/q/onboarding/demo/models/Contact.java
@@ -1,6 +1,20 @@
 package com.q.onboarding.demo.models;
 
 public class Contact {
-  public int Id;
-  public String Name;
+
+  private int Id;
+  private String Name;
+
+  public Contact(int id, String name){
+    this.Id = id;
+    this.Name = name;
+  }
+
+  public int getId() {
+    return Id;
+  }
+
+  public String getName() {
+    return Name;
+  }
 }
--- src/main/java/com/q/onboarding/demo/models/Task.java
+++ src/main/java/com/q/onboarding/demo/models/Task.java
@@ -1,11 +1,19 @@
 package com.q.onboarding.demo.models;
 
 public class Task {
-  public int Id;
-  public String Name;
+  private int Id;
+  private String Name;
 
   public Task(int id, String name){
     this.Id = id;
     this.Name = name;
+  }
+
+  public int getId() {
+    return Id;
+  }
+
+  public String getName() {
+    return Name;
   }
 }
