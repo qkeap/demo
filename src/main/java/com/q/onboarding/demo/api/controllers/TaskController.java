19bf6a170054659c85f7f03eb95b12d338f2a1b5
Q <quintin.robinson@keap.com>
2019-03-28 10:11:40 -0700

    updated controller routing annotations

--- src/main/java/com/q/onboarding/demo/api/controllers/TaskController.java
+++ src/main/java/com/q/onboarding/demo/api/controllers/TaskController.java
@@ -9,10 +9,10 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +36,7 @@ public class TaskController {
         modelConverter.convertTaskDomainModelToDTO(task), HttpStatus.CREATED);
   }
 
-  @RequestMapping("/task")
+  @GetMapping("/task")
   public ResponseEntity<List<TaskDTO>> getTasksForContact(
       @RequestHeader(value = "Authorization") String authorization, @RequestParam long contactId) {
 
